3535172dbac5ba5a2b942fb6f8b1bc8ac2f2d8b4
xuwenli0304 <2796439991@qq.com>
2022-07-02 18:24:09 +0800

    start sb application, return json response

---
+++ src/main/java/Chapter1Main.java
@@ -0,0 +1,32 @@
+//package com.springboot.chapter1.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+
+// @SpringBootApplication
+// @Controller
+// @EnableAutoConfiguration
+@RestController
+@EnableAutoConfiguration
+public class Chapter1Main {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Main.class, args);
+	}
+	
+	@RequestMapping("/test")
+	@ResponseBody
+	public Map<String, String> test() {
+		Map<String, String> map = new HashMap<>();
+		map.put("key1", "value1");
+		return map;
+	}
+}
